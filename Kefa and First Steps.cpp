8e4cf05145e6834aae43b0e09825bd0c2bd3257e
Aman Saxena <65945317+amansaxena1@users.noreply.github.com>
2020-12-27 21:15:37 +0530

    Kefa and First Steps

--- Kefa and First Steps.cpp
+++ Kefa and First Steps.cpp
@@ -6,23 +6,8 @@ using namespace std;
 
 int main()
 {
-    long int n, a, b = 1;
-    vector <long int> v;
-    cin>>n;
-    for(int i = 0; i < n; i++){
-        cin>>a;
-        v.push_back(a);
-    }
-    a = 1;
-    for(int i = 0; i < n-1; i++){
-        if(v[i] <= v[i+1]){
-            a++;
-        }
-        else{
-            b = a;
-            a = 0;
-        }
-    }
-    cout<<b;
+    long long a, b;
+    cin>>a>>b;
+    cout<<((a % 1000000007) + (a % 1000000007)) % 1000000007 ;
     return 0;
 }
